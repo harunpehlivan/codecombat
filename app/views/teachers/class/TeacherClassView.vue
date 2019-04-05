ae8a04f4c3074bb29060e47bf00906d962c1ddde
codeluggage <matias.forbord@gmail.com>
2019-04-05 23:09:01 +0200

    First few steps of Vue breadcrumbs - with state issues

--- app/views/teachers/class/TeacherClassView.vue
+++ app/views/teachers/class/TeacherClassView.vue
@@ -1,12 +1,16 @@
 <template>
     <loading-progress :loading="loading" :progress="progress">
+        <breadcrumbs v-if="!loading" v-bind:links="links"></breadcrumbs>
         <backbone-view v-if="!loading" :backbone-view="backboneViewInstance"></backbone-view>
     </loading-progress>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   import TeacherClassView from 'app/views/courses/TeacherClassView'
   import LoadingProgress from '../../core/LoadingProgress'
+  import Breadcrumbs from '../../common/BreadcrumbComponent'
+  import Classroom from 'models/Classroom'
 
   const BackboneView = {
     props: {
@@ -24,7 +28,8 @@
   export default {
     components: {
       LoadingProgress,
-      BackboneView
+      BackboneView,
+      Breadcrumbs
     },
 
     data: function () {
@@ -35,7 +40,16 @@
         backboneViewInstance: new TeacherClassView(
           { vue: true, readOnly: true },
           this.$route.params.classroomId
-        )
+        ),
+        links: [{
+          href: '/school-administrator',
+          i18n: 'school_administrator.my_teachers'
+        }, {
+          href: `/school-administrator/teachers/${this.$route.teacherId}`,
+          text: this.teacherName
+        }, {
+          text: this.classroomName()
+        }]
       }
     },
 
@@ -50,8 +64,34 @@
 
       updateLoadingProgress: function (progress) {
         this.progress = progress
+      },
+
+      classroomName: function () {
+        console.log('trying to get classroomName:')
+        console.log(this.$route.params.classroomId)
+        const classroom = new Classroom(this.$route.params.classroomId)
+        console.log(classroom)
+        console.log(classroom.get('name'))
+        return classroom.get('name')
       }
     },
+
+    computed: Object.assign({},
+      mapState('schoolAdministrator', [
+        'administratedTeachers'
+      ]),
+
+      {
+        teacherName: () => {
+          console.log('inside teacherName now!')
+          console.log(this.administratedTeachers)
+          const teacher = this.administratedTeachers.find(t => t.id === this.$route.teacherId)
+          console.log(teacher)
+          return teacher.firstName ? `${teacher.firstName} ${teacher.lastName}` : teacher.name
+        }
+      }
+    ),
+
 
     created() {
       this.backboneViewInstance.on('loading:show', this.showLoading)
